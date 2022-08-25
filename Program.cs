b94b88b0b5baa0a6eeaf4d9a4f54a083b2e7f25c
Strayko <strayko_lol@windowslive.com>
2022-08-25 22:30:29 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+var s1 = Singleton.GetInstance();
+var s2 = Singleton.GetInstance();
+var s3 = Singleton.GetInstance();
+
+Console.WriteLine(s1 == s2);
+Console.WriteLine(s2 == s3);
---
+++ Singleton.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2;
+
+public class Singleton
+{
+    private static Singleton? _instance;
+
+    public Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+            _instance = new Singleton();
+
+        return _instance;
+    }
+}
